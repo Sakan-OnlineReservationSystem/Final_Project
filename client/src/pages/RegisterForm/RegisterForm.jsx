bb230eb20cd34e6aa2f23213271cfcc482beb144
Abdelrahman Ibrahim Abdelhalim Saad <76453265+abdelrahmansaad24@users.noreply.github.com>
2024-02-18 22:45:25 +0200

    Update RegisterForm.jsx

--- client/src/pages/RegisterForm/RegisterForm.jsx
+++ client/src/pages/RegisterForm/RegisterForm.jsx
@@ -145,7 +145,7 @@ const RegisterForm = () => {
                     value={value}
                     onChange={
                       console.log(value)
-                      data.Phone = value
+                      //data.Phone = value
                       setValue
                     }/>) : field.label == "Country"? (
                       <CountrySelect className={`border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-green-500 ${
