b2b598cc1347759a5b041d93684eec49859b75ee
Abdelrahman Ibrahim Abdelhalim Saad <76453265+abdelrahmansaad24@users.noreply.github.com>
2024-02-19 01:49:10 +0200

    Update RegisterForm.jsx

--- client/src/pages/RegisterForm/RegisterForm.jsx
+++ client/src/pages/RegisterForm/RegisterForm.jsx
@@ -167,7 +167,7 @@ const RegisterForm = () => {
                     onChange={(e) => {
                         setPhone(e)
                     }} required
-                       /> {phoneError? <p>Phone number is required and valid</p> : <></>}) : field.label == "Country"? (
+                       /> <>{phoneError? <p>Phone number is required and valid</p> : <></>}</> ) : field.label == "Country"? (
                       <CountrySelect className={`border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-green-500 ${
                       field.gridCols === 2 ? "md:w-full" : ""
                       }`}
