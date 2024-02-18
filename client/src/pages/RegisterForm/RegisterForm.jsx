9281504b2c4bb9f5109ef52d701fb08720f0142c
Abdelrahman Ibrahim Abdelhalim Saad <76453265+abdelrahmansaad24@users.noreply.github.com>
2024-02-19 00:44:02 +0200

    Update RegisterForm.jsx

--- client/src/pages/RegisterForm/RegisterForm.jsx
+++ client/src/pages/RegisterForm/RegisterForm.jsx
@@ -146,7 +146,7 @@ const RegisterForm = () => {
                     value={value}
                     onChange={(e) => {
                         setPhone(e)
-                    }} required
+                    }} 
                        />) : field.label == "Country"? (
                       <CountrySelect className={`border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-green-500 ${
                       field.gridCols === 2 ? "md:w-full" : ""
