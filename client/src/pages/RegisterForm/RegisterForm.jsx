068bb1d808b161519958357d6483c77573fa9971
Abdelrahman Ibrahim Abdelhalim Saad <76453265+abdelrahmansaad24@users.noreply.github.com>
2024-02-18 22:37:59 +0200

    Update RegisterForm.jsx

--- client/src/pages/RegisterForm/RegisterForm.jsx
+++ client/src/pages/RegisterForm/RegisterForm.jsx
@@ -82,9 +82,6 @@ const RegisterForm = () => {
 
   const onSubmit = async (data) => {
     // Password setting
-    data.Phone = formatPhoneNumber(value)
-
-
     setPassword(data.password);
     setConfirmPassword(data.confirmpassword);
     // check passwords match
@@ -146,7 +143,11 @@ const RegisterForm = () => {
                     }`}
                     placeholder="Enter phone number"
                     value={value}
-                    onChange={setValue}/>) : field.label == "Country"? (
+                    onChange={
+                      console.log(value)
+                      data.Phone = value
+                      setValue
+                    }/>) : field.label == "Country"? (
                       <CountrySelect className={`border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-green-500 ${
                       field.gridCols === 2 ? "md:w-full" : ""
                       }`}
