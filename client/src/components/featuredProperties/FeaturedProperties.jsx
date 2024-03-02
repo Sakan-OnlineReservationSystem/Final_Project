60266352259213697b87500487a950112c136c88
Ayman-Mohamed-abotaha <eng.ayman.mohamed.2001@gmail.com>
2024-03-02 17:14:38 +0200

    styles

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -19,16 +19,16 @@ const FeaturedProperties = () => {
           {data && data.length !== 663 && data.length !== 0 ? (
             data.map((item) => (
               <button className="fpItem" key={item._id}>
-                <img src={item.photos[0]} alt="" className="fpImg" />
                 <Link style={{ textAlign: "start" }} to={`/hotels/${item._id}`}>
+                  <img src={item.photos[0]} alt="" className="fpImg" />
                   <span className="fpName">{item.name}</span>
                 </Link>
+                <span>{item.numberOfStars} stars</span>
                 <span className="fpCity">{item.city}</span>
 
                 {item.rating && (
                   <div className="fpRating">
                     <button>{item.rating}</button>
-                    <span>{item.numberOfStars}</span>
                   </div>
                 )}
                 <span className="fpPrice">
--- client/src/pages/Chat_bot/config.js
+++ client/src/pages/Chat_bot/config.js
@@ -1,9 +1,8 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
+import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello, how can i help you ? ....`)],
-  botName:"Scan Bot",
+  initialMessages: [createChatBotMessage(`Hello, how can i help you ?`)],
+  botName: "Scan Bot",
 };
 
 export default config;
-
