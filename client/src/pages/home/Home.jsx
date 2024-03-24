35848d12a7f399e822d0907d02e6040582432d23
Eng-Ayman-Mohamed <eng.ayman.mohamed.2001@gmail.com>
2024-03-24 15:22:52 +0200

    hotel page suspense

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       <Navbar />
       <Header />
       <div className="homeContainer">
-        <Featured />
+        <Featured featured={true} />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
         <h1 className="homeTitle">Recommended for you</h1>
