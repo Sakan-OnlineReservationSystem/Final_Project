886dfe0047a28f01926631d3c467f1f45fed3f76
Hassan <ha4900109@gmail.com>
2024-04-24 01:19:13 +0200

    update

--- api/controllers/payment.js
+++ api/controllers/payment.js
@@ -310,6 +310,7 @@ exports.sayHello = async (req, res, next) => {
 };
 
 exports.webhookCheckout = async (req, res, next) => {
+  const data = req.body;
   if (data.event_type == "CHECKOUT.ORDER.COMPLETED") bookingCheckout(req.body);
   res.status(200).json({ received: true });
 };
