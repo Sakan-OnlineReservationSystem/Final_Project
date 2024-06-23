e0c0d8c33813af3d4f08387a33f4fd159f1c7f87
a7med3z <es-ahmed.ezkak24a@alexu.edu.eg>
2024-06-23 19:59:49 +0300

    fix booking

--- api/routes/booking.js
+++ api/routes/booking.js
@@ -14,11 +14,11 @@ const { protect, isNormalUser, isOwner } = require("../controllers/auth.js");
 
 const router = express.Router();
 
+router.get("/reservations", protect, getUserRerservations);
 router.post("/", protect, hotelContainRoomNumber, isRoomAvailable, createBooking);
 router.get("/:id", protect, isBookingOwner, getBooking); // id => bookingID
 
 router.delete("/:id", protect, isBookingOwner, deleteBooking); // cancel reservation, id => bookingID
 router.post("/webHook", webhookCheckout);
-router.get("/reservations", protect, getUserRerservations);
 
 module.exports = router;
